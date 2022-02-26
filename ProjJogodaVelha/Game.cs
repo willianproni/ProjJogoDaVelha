0b116c4226c192b0bf6e8aa6d46fc0e992d059d4
Willian Proni <wifepro@hotmail.com>
2022-02-26 15:26:17 -0300

    Adicionar arquivos de projeto.

---
+++ ProjJogodaVelha/Game.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjJogodaVelha
+{
+    internal class Game
+    {
+        public char[,] Jogo { get; set; }
+
+        public char Jogador { get; set; }
+
+        public Game(char[,] jogo)
+        {
+            Jogo = jogo;
+            Jogador = 'X';
+
+        }
+
+        public void Start()
+        {
+            int cont = 1;
+            int jogador = 1;
+            Console.Clear();
+            Console.WriteLine($"-_-_-RODADA {cont++}!!-_-_-\n");
+            do
+            {
+                if (jogador > 2)
+                {
+                    jogador = 1;
+                }
+                Imprimir_Jogo();
+                Console.WriteLine($"Vez do Jogador {jogador}:");
+                jogador++;
+                Console.WriteLine("\nEscolha a linha: ");
+                int linha = int.Parse(Console.ReadLine());
+                Console.WriteLine("Escolha a coluna: ");
+                int coluna = int.Parse(Console.ReadLine());
+                if (Jogo[linha, coluna] != 'X' || Jogo[linha, coluna] != 'O')
+                {
+                    for (int l = linha; l <= linha; l++)
+                    {
+                        for (int c = coluna; c <= coluna; c++)
+                        {
+                            Jogo[l, c] = Jogador;
+                            Imprimir_Jogo();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Escolha outra casa!");
+                }
+
+            } while (cont != 9);
+        }
+
+        public void Imprimir_Jogo()
+        {
+            Console.Clear();
+            Console.WriteLine("-_-_-_-_Situação do Jogo_-_-_-_-_-\n");
+
+            int cont = 0;
+            for (int l = 0; l < Jogo.GetLength(0); l++)
+            {
+                for (int c = 0; c < Jogo.GetLength(1); c++)
+                {
+                    Console.Write("\t" + Jogo[l, c] + "  ");
+                    cont++;
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+            
+        }
+
+        public void VerificarPosition()
+        {
+
+        }
+    }
+}
---
+++ ProjJogodaVelha/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ProjJogodaVelha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } });
+            int ops;
+
+            do
+            {
+                Menu();
+                ops = int.Parse(Console.ReadLine());
+                switch (ops)
+                {
+                    
+                    case 1:
+                        game.Start();
+                        break;
+                    case 2:
+                        game.Imprimir_Jogo();
+                        break;
+                    default:
+                        Console.WriteLine("Digite uma opção válida!");
+                        break;
+                }
+            } while (ops != 3);
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("-------------Jogo da Velha--------------\n" +
+                              "\n\t[1] - Start Game" +
+                              "\n\t[2] - View Game" +
+                              "\n\n----------------------------------------");
+            Console.Write("Opção: ");
+        }
+    }
+}
